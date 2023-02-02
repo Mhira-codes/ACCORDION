aee050667df61b044bdd8bd1771596ce4e121825
mhira-codes <alhassansamiratu991@gmail.com>
2023-02-02 17:24:17 +0000

    data.js file

---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export const questionsDetails =[{
+    id:1,
+    title:"Who invented the first modern tractor?",
+    details:"In 1901, the first commercial lightweight petrol tractor was invented by Dan Albone."
+   
+},
+{
+    id:2,
+    title:"What is the difference between a Mini and Utility tractor?",
+    details:"Mini tractors are smaller in size, having 11.1 Hp -36 Hp, ideal for small scale farming and land mowing tasks. Utility tractors are bigger in size, have hp ranging between 40 hp to 100+ hp, ideal for high-end farming tasks"
+},
+{
+    id:3,
+    title:" How do you maintain a tractor?",
+    details:" Regularly checking the radiator-fluid level, tractor’s oil, tire pressure, air filter helps to maintain a farm tractor."
+
+},
+{
+    id:4,
+    title:"How many different types of farming methods are there in India?",
+    details:"Organic farming, subsistence farming, commercial farming are popular farming methods used in India. However, depending on geographical conditions, production demand, level of technology and labour, farming can be based on ley farming, horticulture, agroforestry, etc."
+},
+{
+    id:5,
+    title:"What is a Zaid crop with an example?",
+    details:"They are summer season crops, grown for short periods between March to June"
+},
+{
+    id:6,
+    title:"What is the difference between agriculture and farming?",
+    details:"Agriculture is a vast term that involves growing crops and raising animals that provide food and utility resources. It also includes R&D, production, distribution, advanced technology, and more. While farming is part of agriculture that is plant or modern science based, involving cultivating soil to grow crops and rearing animals for their by-products."
+}
+,{
+    id:7,
+    title:" What are the benefits of organic farming?",
+    details:"Organic farming prevents the use of pesticides, herbicides and other harmful chemicals. Due to organic farming, plants and insects will survive."
+}
+]
